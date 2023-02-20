448597a0ebb07a36865358260eb798a0d853c0a6
Yevhen <112542705+K-Yevhen@users.noreply.github.com>
2023-02-20 10:21:51 +0100

    keep modifying

--- DiscordBot/gatewayprotocol.py
+++ DiscordBot/gatewayprotocol.py
@@ -3,6 +3,11 @@ from gateway import GatewayCon
 LIB_NAME = 'tmtc-dispy'
 
 class Gateway(GatewayCon):
+
+    def __init__(self, token):
+        super().__init__(token)
+        self._handlers = {}
+
     async def handle_message(self, msg):
         print(msg)
         if msg.op == 10:
@@ -10,7 +15,7 @@ class Gateway(GatewayCon):
             self._pulse = msg.data.heartbeat_interval / 1000
             identity = {
                 "op": 2,
-                "d":{
+                "d": {
                     "token": self._token,
                     "intents": 1 << 10,
                     "properties": {
@@ -20,13 +25,21 @@ class Gateway(GatewayCon):
                     },
                 }
             }
-            self.send(identity)
+            await self.send(identity)
+            print('done identity')
+        elif msg.op == 0:
+            event = msg.name.lower()
+            if event in self._handlers:
+                self._handlers[event](msg)
+            else:
+                print(f"unhandled event {event}")
         else:
-            raise Exception(f"unkonwn op in message{msg.op}")
+            raise Exception(f"unknown op in message{msg.op}")
 
 if __name__ == "__main__":
     token = "foo"
     with open(".token") as token_file:
         token = token_file.read()[:-1]
         g = Gateway(token)
+        g._handlers["ready"] = lambda x: print("ready")
         g.run()
