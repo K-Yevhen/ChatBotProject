6bd908d20a035c62cebf04f6cf72fb189f916216
Yevhen <112542705+K-Yevhen@users.noreply.github.com>
2023-02-23 12:07:28 +0100

    finished telegram_bot connecting with openai
    small project which is connecting telegram bot with openAI.

--- Telegram-ChatBot/ChatGPT-Telegram/creating_bot.py
+++ Telegram-ChatBot/ChatGPT-Telegram/creating_bot.py
@@ -3,11 +3,30 @@ import openai
 from aiogram import Bot, types
 from aiogram.dispatcher import Dispatcher
 from aiogram.utils import executor
+import secrets_token
 
-token = "5849801643:AAGHnfrc6CNG4bRG8ztlhnIx6a9dQgaIiIs"
-openai.api_key = "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"
+token = secrets_token.telegram_token
+openai.api_key = secrets_token.open_ai_token
 
 bot = Bot(token)
 dispatcher = Dispatcher(bot)
 
-print(openai.Model.list())
+# print(openai.Model.list())
+
+
+@dispatcher.message_handler()
+async def send(message: types.Message):
+    response = openai.Completion.create(
+        model="text-davinci-003",
+        prompt=message.text,
+        temperature=0.9,
+        max_tokens=1000,
+        top_p=1.0,
+        frequency_penalty=0.0,
+        presence_penalty=0.6,
+        stop=[" Human:", " AI:"]
+    )
+
+    await message.answer(response['choices'][0]['text'])
+
+executor.start_polling(dispatcher, skip_updates=True)
